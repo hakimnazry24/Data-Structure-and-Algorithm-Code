26aabcfb0c70696b855162aa074f378f7a880a89
Hakim <hakimnazry@gmail.com>
2023-05-17 22:51:53 +0800

    adding comments and notes

--- dijkstra_alg.py
+++ dijkstra_alg.py
@@ -1,3 +1,17 @@
+# Dijkstra algorithm
+# - Djikstra algorithm allows us to find the shortest path between starting vertex to all vertices in a graph
+# 
+#
+# Steps
+# 1) Pick starting node
+# 2) Initialize table of distances
+# 3) Examine edges leaving starting vertex
+# 3) Update the distance to those vertices
+# 4) Pick the smallest edge/distances of which the vertex hasnt been chosen
+# 5) Cross the visited node in the unvisited node
+# 6) Examine the current node, looking for all edges, then choose the smallest one
+# 7) Repeat for all unvisited node
+
 import heapq
 
 def dijkstra(graph, starting_vertex):
@@ -24,6 +38,8 @@ graph = {
     'B': {'D':5},
     'C': {'D':8},
     'D':{},
+    
 }
 
 print(dijkstra(graph, 'A'))
+print(graph['A'].items())
